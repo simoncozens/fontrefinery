b2f5c4a2a3143d142269bf00b8ef12b8b679b0a4
Simon Cozens <simon@simon-cozens.org>
2024-06-05 13:43:26 +0100

    Various

--- Lib/fontrefinery/fixes/googlefonts.py
+++ Lib/fontrefinery/fixes/googlefonts.py
@@ -1,12 +1,16 @@
+import os
 import re
 
-from fontbakery.checks.googlefonts.conditions import family_metadata, font_metadata
+# Conditions
+import fontbakery.checks.googlefonts.conditions
+import fontbakery.checks.googlefonts.description
+import fontbakery.checks.googlefonts.license
+
 from fontbakery.checks.googlefonts.constants import EXPECTED_COPYRIGHT_PATTERN
-from fontbakery.checks.googlefonts.license import license_contents, license_path
 from fontbakery.constants import NameID
 from fontTools.misc.timeTools import timestampToString
 from gftools.fix import fix_ofl_license
-from gftools.utils import font_stylename
+from gftools.utils import font_stylename, remove_url_prefix
 from gftools.util.google_fonts import WriteMetadata
 
 from fontrefinery.testable import Font
@@ -25,7 +29,7 @@ def fix_fs_type(font: Font, _context) -> FixResult:
         "com.google.fonts/check/fsselection",
     ]
 )
-def fix_fs_selection(font: Font) -> FixResult:
+def fix_fs_selection(font: Font, _context) -> FixResult:
     """Fix the OS/2 table's fsSelection so it conforms to GF's supported
     styles table:
     https://github.com/googlefonts/gf-docs/tree/main/Spec#supported-styles
@@ -52,13 +56,21 @@ def fix_fs_selection(font: Font) -> FixResult:
     id=[
         "com.google.fonts/check/font_copyright",
         "com.google.fonts/check/license/OFL_copyright",
+        "com.google.fonts/check/metadata/consistent_repo_urls",
     ]
 )
 def fix_font_copyright(font, _context) -> FixResult:
     if not font.family_metadata:
-        return False, ["Metadata is missing"]
+        return False, ["[bright_red]Metadata is missing[/bright_red]"]
+    if not font.font_metadata:
+        return False, [
+            "[bright_red]Metadata present but font metadata missing[/bright_red]"
+        ]
     if not font.family_metadata.source.repository_url:
-        return False, ["Repository URL is missing in METADATA.pb"]
+        return False, ["[red]Repository URL is missing in METADATA.pb[/red]"]
+
+    if "reserved font name" in font.font_metadata.copyright.lower():
+        return False, ["[red]Reserved font name found in copyright[/red]"]
 
     font_year = timestampToString(font.ttFont["head"].created)[-4:]
     expected_copyright = f"Copyright {font_year}, the {font.family_metadata.name} Project Authors ({font.family_metadata.source.repository_url})"
@@ -79,7 +91,42 @@ def fix_font_copyright(font, _context) -> FixResult:
         EXPECTED_COPYRIGHT_PATTERN, font.license_contents.lower()
     ):
         with open(font.license_path, "w", encoding="utf-8") as fp:
-            fp.write(fix_ofl_license(font.ttFont) + "\n")
-        messages.append("OFL rewritten")
+            try:
+                fp.write(fix_ofl_license(font.ttFont) + "\n")
+                messages.append("OFL rewritten")
+            except ValueError:
+                messages.append("[red]OFL could not be rewritten (RFN?)[/red]")
 
     return font_changed, messages
+
+
+@fix(
+    id=[
+        "com.google.fonts/check/description/git_url",
+    ]
+)
+def fix_description_git_url(font, _context) -> FixResult:
+    if not font.family_metadata or not font.family_metadata.source.repository_url:
+        return False, []
+
+    if not font.description or font.description_html is None:
+        return False, ["[bright_red]Description is missing[/bright_red]"]
+
+    git_urls = []
+    for a_href in font.description_html.iterfind(".//a[@href]"):
+        link = a_href.get("href")
+        if "://git" in link:
+            git_urls.append(link)
+
+    if git_urls:
+        return False, []
+    github_url = font.family_metadata.source.repository_url
+    description = font.description
+    human_url = remove_url_prefix(github_url)
+    description += (
+        f'\n<p>To contribute, please see <a href="{github_url}">{human_url}</a>.</p>'
+    )
+    descfile = os.path.join(os.path.dirname(font.file), "DESCRIPTION.en_us.html")
+    with open(descfile, "w", encoding="utf-8") as f:
+        f.write(description)
+    return False, ["Git URL added to DESCRIPTION"]
